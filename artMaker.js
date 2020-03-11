374f2ab2e57dc568e41df5863e46440bc33900c3
Alex Vitiello <alexv@stripe.com>
2020-03-11 16:14:56 -0700

    basic art pixel maker

---
+++ artMaker.js
@@ -0,0 +1,40 @@
+
+window.onload = function(){
+    
+    let container = document.querySelector('#canvas')
+    let pallet = document.querySelector('#pallet-container')
+
+
+    let colorChoices = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+    for(let color of colorChoices) {
+        let palletBox = document.createElement('span')
+        palletBox.className = "pallet-box"
+        palletBox.style.backgroundColor = color
+        palletBox.style.height = '10px'
+        palletBox.style.width = '10px'
+        palletBox.style.border = '2px solid black';
+
+        palletBox.addEventListener('click', function(event){
+            brushColor = palletBox.style.backgroundColor
+        });
+
+        pallet.appendChild(palletBox);
+
+    }
+
+    for(let i=0; i < 400; i++){
+        let box = document.createElement('span')
+        box.style.width = '8px'
+        box.style.height = '8px'
+        box.style.border = '1px solid black'
+
+     box.addEventListener('click', function(event) {
+        box.style.backgroundColor = brushColor
+     })
+     container.appendChild(box)
+    }   
+
+}
+
+
+
